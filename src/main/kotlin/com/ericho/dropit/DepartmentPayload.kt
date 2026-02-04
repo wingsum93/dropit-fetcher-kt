972fea4788ee15e0c52efc59e0467c7743341ccc
Eric Ho <wingsum.developer@gmail.com>
2026-02-04 18:09:02 +0800

    add class

---
+++ src/main/kotlin/com/ericho/dropit/DepartmentPayload.kt
@@ -0,0 +1,37 @@
+package com.ericho.dropit
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DepartmentPayload(
+    val total: Int,
+    val departments: List<Department>
+)
+
+@Serializable
+data class Department(
+    val id: String,
+    val count: Int,
+    val sequence: Int,
+    val name: String,
+    @SerialName("parent_id")
+    val parentId: String? = null,
+    val identifier: String,
+    @SerialName("_sequence")
+    val internalSequence: Int? = null,
+    @SerialName("store_id")
+    val storeId: String,
+    @SerialName("store_depth")
+    val storeDepth: Int,
+    @SerialName("type_id")
+    val typeId: String,
+    val path: String,
+    val lineage: List<String>,
+    @SerialName("canonical_url")
+    val canonicalUrl: String,
+    @SerialName("master_taxonomy")
+    val masterTaxonomy: String? = null,
+    @SerialName("is_red_department")
+    val isRedDepartment: Boolean? = null
+)
